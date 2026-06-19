6c5191ce20db1d5f8d0931613b9030fa170ff345
ItzSanket99 <jagtapsanket58@gmail.com>
2026-06-19 21:59:47 +0530

    Initial Spring Boot Project Setup

---
+++ ai-content-guard/src/main/java/com/sanket/aicontentguard/AiContentGuardApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.aicontentguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiContentGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiContentGuardApplication.class, args);
+	}
+
+}
