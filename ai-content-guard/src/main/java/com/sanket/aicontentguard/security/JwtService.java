23191ce0eff30f022173f6b8e13e21cfae250822
ItzSanket99 <jagtapsanket58@gmail.com>
2026-06-20 11:28:52 +0530

    implement jwt authentication filter and secure endpoints

--- ai-content-guard/src/main/java/com/sanket/aicontentguard/security/JwtService.java
+++ ai-content-guard/src/main/java/com/sanket/aicontentguard/security/JwtService.java
@@ -1,13 +1,16 @@
 package com.sanket.aicontentguard.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -39,5 +42,44 @@ public class JwtService {
                         getSigningKey()
                 )
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(
+            String token,
+            Function<Claims, T> claimsResolver) {
+
+        Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith((SecretKey) getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean isTokenValid(
+            String token,
+            String email) {
+
+        String username = extractUsername(token);
+
+        return username.equals(email)
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(
+                token,
+                Claims::getExpiration
+        ).before(new Date());
     }
 }
